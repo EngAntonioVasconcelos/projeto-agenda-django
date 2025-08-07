313928646e709cc9c05422f51e5a34ac08c57b40
Antonio Vasconselos <fatimapaulogomes20@gmail.com>
2025-08-07 19:11:05 +0100

    adicionando função CATEGORIA

--- contact/admin.py
+++ contact/admin.py
@@ -4,13 +4,18 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'created_date',) 
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'created_date','birthday',) 
     ordering = ('-id',)
     search_fields = ('id','first_name', 'last_name', 'email')
     list_filter = ('created_date',)
     list_per_page = 10
     list_max_show_all = 1000
-    list_editable = ('first_name', 'last_name', 'email', )
+    list_editable = ('first_name', 'last_name', 'email','birthday')
     list_display_links = ('id', 'phone',)
+    date_hierarchy = 'birthday'
 
-   
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ( 'name',)
+    ordering = ('-id',)
+    
--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -12,7 +18,10 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(upload_to='contact_pictures/%Y/%m/%d/', blank=True)
-
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='contacts', blank=True, null=True)
+    birthday = models.DateField(verbose_name='Data de Nascimento', blank=True, null=True)
+    
+    
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
