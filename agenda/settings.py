a529823746f8f90a213d577d8fe7e7db521df27f
Antonio Vasconselos <fatimapaulogomes20@gmail.com>
2025-08-07 17:58:32 +0100

    Atualização para fazer colocar imagem do contato gravado

--- agenda/settings.py
+++ agenda/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Lisbon'
 
 USE_I18N = True
 
@@ -119,10 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS =STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_dev'),  
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_dist') 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
