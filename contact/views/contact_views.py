06cad87d82e7c4e932d468cca375c4f434e436ac
Antonio Vasconselos <fatimapaulogomes20@gmail.com>
2025-08-08 19:44:34 +0100

    Trabalhei na parte frontal do projeto

---
+++ contact/views/contact_views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from contact.models import Contact
+
+def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+    print(contacts.query)
+    return render(request, 'contact/index.html', {'contacts': contacts})
